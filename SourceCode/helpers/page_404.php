d7491c8513aef9a47efe18180be7cae3f5860e9c
bayusantoso.mail@gmail.com <bayusantoso.mail@gmail.com@be97e1e9-59b8-1903-9059-09828cfd9d99>
2014-11-17 09:48:08 +0000

    [Bayu Santoso] - Menambahkan page not found

--- SourceCode/helpers/page_404.php
+++ SourceCode/helpers/page_404.php
@@ -1,1 +1,10 @@
-<h1>Page Not Found</h1>
+<div class="form-actions">
+    <h1>Page Not Found</h1>
+    <a id="back-button" href="javascript:void()">Back</a>
+    <script type="text/javascript">
+    $("#back-button").click(function() { return goBack(); });
+    function goBack() {
+        window.history.back();
+    }
+    </script>
+</div>
